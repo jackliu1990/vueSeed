78de1ef71368b901f865b6b2115d9a18a693cf5e
liufeng <1021563889@qq.com>
2019-01-24 22:43:35 +0800

    提交webpack typescript的SDK开发示例

---
+++ caf/test.js
@@ -0,0 +1,3 @@
+/**
+ * Created by liufeng on 2019/1/24.
+ */
---
+++ tw/src/Test.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by liufeng on 2019/1/24.
+ */
+export class Test{
+    name:string;
+    constructor(name:string){
+        this.name = name;
+    }
+
+    getName(){
+        return this.name;
+    }
+}
