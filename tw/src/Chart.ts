bc3e0838e9abbe82e7f23498ddc54e845f13e9ee
liufeng <1021563889@qq.com>
2019-01-27 20:42:53 +0800

    提交代码

---
+++ tw/src/Chart.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by liufeng on 2019/1/27.
+ */
+export class Chart{
+    chart:string;
+    constructor(chart:string){
+        this.chart = chart;
+    }
+
+    getChart() {
+        return this.chart;
+    }
+}
